4d77b7011c5a78d83d026c1f8328f6ece0c8fc0c
Kristyna Podana <kristyna.zam@gmail.com>
2023-10-24 13:56:07 +0200

    Novy soubor

--- ukol-01.js
+++ ukol-01.js
@@ -5,3 +5,5 @@ const email = {
     userFirstName: firstName.slice(0, 3),
     userSurname: surname.slice(0, 5),
 };
+
+document.body.innerHTML += `<p> ${userSurname + userFirstName} @fit.cvut.cz</p>`;
--- ukol-02.js
+++ ukol-02.js
@@ -1,16 +1,18 @@
 const pocet = prompt("Počet kusů:");
+let cenaKs = 0;
 
-const cenaKs = "";
 if (pocet < 50) {
     cenaKs = 300;
-} else if (pocet >= 50) {
+} else if (pocet >= 50 && pocet < 200) {
     cenaKs = 250;
-} else if (pocet >= 200) {
+} else if (pocet >= 200 && pocet < 500) {
     cenaKs = 200;
-} else if (pocet >= 500) {
+} else if (pocet >= 500 && pocet < 1000) {
     cenaKs = 150;
 } else if (pocet >= 1000) {
     cenaKs = 120;
 };
 
-document.body.innerHTML += `<p>Celková cena objednávky: ${cenaKs * pocet} Kč.</p>`
+const cenaCelkem = cenaKs * pocet;
+
+document.body.innerHTML += `<p>Celková cena objednávky: ${cenaCelkem} Kč.</p>`
