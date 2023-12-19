6261e50c4d77aa22d2ba1bd74b15b55ee43e582f
Kristyna Podana <kristyna.zam@gmail.com>
2023-12-19 19:28:29 +0100

    Doplnene zadani

--- ukol-03.js
+++ ukol-03.js
@@ -1,3 +1,10 @@
+/* parsování data
+Bez použití knihovny Day.js napište funkci parseDate, která na vstupu obdrží řetězec obsahující datum ve formátu DD.MM.YYYY a vrátí objekt s jednotlivýnu částmi zadaného data. Příklad použití:
+
+parseDate('12.03.2015'); // → { day: 12, month: 3, year: 2015 }
+parseDate('06.04.2021'); // → { day: 6, month: 4, year: 2021 }
+*/
+
 const parseDate = () => {
     const inputDate = prompt("Zadej datum ve formátu DD.MM.YYYY:");
     const date = {
--- ukol-05.js
+++ ukol-05.js
@@ -1,3 +1,16 @@
+/* výplata
+Vytvořte funkci salary se třemi parametry
+
+wage - hrubá mzda v korunách za hodinu
+hours - kolik hodin denně průměrně pracujete
+days - kolik dní v měsící průměrně pracujete
+Funkce spočítá vaši hrubou měsíční mzdu v celých korunách.
+
+Dále vytvořte funkci taxed, která na vstupu obdrží částku a procento zdanění, a vrátí částku v celých korunách zdaněnou podle zadaných procent.
+
+Použítím funkcí salary a taxed spočítejte svoji měsíční mzdu po 15% zdanění.
+*/
+
 const salary = (wage, hours, days) => {
     const mySalary = wage * hours * days
     document.body.innerHTML += `<p>Hrubá měsíční mzda: ${mySalary} Kč</p>`
